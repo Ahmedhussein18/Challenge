4fbaff2e54dda047e9b75df02dc2e1d54ccdac6e
Ubuntu <ubuntu@ip-172-31-38-69.ec2.internal>
2024-10-08 12:30:20 +0000

    Challenge Commit

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,13 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-})
-// module.exports = {
-//   devServer: {
-//     open: process.platform === 'darwin',
-//     host: '0.0.0.0',
-//     port: 443, // CHANGE YOUR PORT HERE!
-//     https: true,
-//     hotOnly: false,
-//   },
-// }
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+  },
+  publicRuntimeConfig: {
+    apiBaseUrl: 'http://3.92.232.51/api'
+  }
+});
